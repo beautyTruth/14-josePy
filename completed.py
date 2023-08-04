81f9036567dae7e67fc7e7d173ef7f53f34f340d
Phil Hasenkamp <89226935+beautyTruth@users.noreply.github.com>
2023-08-04 10:46:59 -0500

    complete section 14

---
+++ completed.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+pi = math.pi
+blender = random.randint(7,77)
+
+def schpadoinkle(x,z):
+    print("I still don't really know what I'm doing in this program but I can follow along really well")
+
+    return x*z/blender
+
+ilovewalking = schpadoinkle(7,77)
+
+print(ilovewalking/blender)
+
+print(schpadoinkle(5,6))
